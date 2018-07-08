4303b38ffb96da8a885b23866f4a9d9ee823a188
Nan Zhu <nanzhu@uber.com>
2018-07-07 21:55:44 -0700

    add new notebook

--- tensorflow_tutorial/utils.py
+++ tensorflow_tutorial/utils.py
@@ -91,3 +91,18 @@ def download_mnist(path):
         download_url = os.path.join(url, filename)
         local_dest = os.path.join(path, filename)
         download_one_file(download_url, local_dest, byte, True)
+
+def read_birth_life_data(filename):
+    """
+    Read in birth_life_2010.txt and return:
+    data in the form of NumPy array
+    n_samples: number of samples
+    """
+    text = open(filename, 'r').readlines()[1:]
+    data = [line[:-1].split('\t') for line in text]
+    births = [float(line[1]) for line in data]
+    lifes = [float(line[2]) for line in data]
+    data = list(zip(births, lifes))
+    n_samples = len(data)
+    data = np.asarray(data, dtype=np.float32)
+    return data, n_samples
